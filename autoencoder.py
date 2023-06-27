6fe9df90221edd0ddd23a622df0478878c9ca9e3
Max <shidoutsuruya@gmail.com>
2023-06-27 22:51:36 +0800

    autoencoder end

--- autoencoder.py
+++ autoencoder.py
@@ -4,14 +4,17 @@ from tensorflow.keras.layers import Reshape,Conv2DTranspose
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras import backend as K
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from typing import *
+import os
 #global variables
 shape=0
 image_size=28
+BASE=os.path.join(os.path.dirname(__file__))
 def data_load():
     (x_train,_),(x_test,_)=mnist.load_data()
     #resize and normalize
@@ -55,7 +58,7 @@ def encoder(input_shape,
                to_file="encoder.png",
                show_shapes=True)
     return encoder
-def decoder(kernel_sizes:int=3,
+def decoder(kernel_size:int=3,
             layer_filters:List[int]=[32,64],
             latent_dim:int=6):
     latent_inputs=Input(shape=(latent_dim,),
@@ -64,12 +67,12 @@ def decoder(kernel_sizes:int=3,
     x=Reshape((shape[1],shape[2],shape[3]))(x)
     for filters in layer_filters[::-1]:
         x=Conv2DTranspose(filters=filters,
-                          kernel_size=(3,3),
+                          kernel_size=kernel_size,
                           activation=tf.keras.activations.relu,
                           strides=2,
                           padding="same")(x)
     outputs=Conv2DTranspose(filters=1,
-                            kernel_size=(3,3),
+                            kernel_size=kernel_size,
                             activation=tf.keras.activations.sigmoid,
                             padding="same",
                             name="decoder_output")(x)
@@ -77,7 +80,15 @@ def decoder(kernel_sizes:int=3,
     decoder.summary()
     plot_model(decoder,to_file="decoder.png",show_shapes=True)
     return decoder
-def autoencoder(input_shape=(image_size,image_size,1)):
+def autoencoder(input_shape:Tuple[int,int,int]=(image_size,image_size,1)):
+    """combine with encoder and decoder
+
+    Args:
+        input_shape (Tuple[int,int,int], optional): input shape. Defaults to (image_size,image_size,1).
+
+    Returns:
+        coder(model):autoencoder 
+    """
     model1=encoder(input_shape)
     model2=decoder()
     outputs=model2(model1.output)
@@ -85,7 +96,38 @@ def autoencoder(input_shape=(image_size,image_size,1)):
                 outputs=outputs)
     plot_model(coder,to_file="coder.png",show_shapes=True)
     return coder
+def train(x_train,x_test):
+    model=autoencoder()
+    #save model
+    save_dir=os.path.join(BASE,"model_saves")
+    model_name=f"autocoder.h5"
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    filepath=os.path.join(save_dir,model_name)
+    #use this to save the best model
+    checkpoint=ModelCheckpoint(filepath=filepath,
+                               monitor="loss",
+                               verbose=1,
+                               save_best_only=True
+                               ) 
+    #training
+    model.compile(loss=tf.keras.losses.mse,
+                  optimizer=tf.keras.optimizers.Adam())
+    model.fit(x_train,x_train,validation_data=(x_test,x_test),epochs=1,
+              batch_size=32,callbacks=[checkpoint,])
+def evalutate(x_gold:np.array,
+              model_path:str=r"model_saves\autocoder.h5"):
+    x_gold=np.expand_dims(x_gold,axis=-1)
+    model=tf.keras.models.load_model(model_path)
+    x_decode=model.predict(x_gold)
+    imgs=np.concatenate([x_gold[:8],x_decode[:8]])
+    imgs=imgs.reshape((4,4,image_size,image_size))
+    imgs=np.vstack([np.hstack(i) for i in imgs])
     
+    plt.imshow(imgs)
+    plt.show()
+    return x_decode
 if __name__ == "__main__":
-    autoencoder()
+    x_train,x_test=data_load()
+    a=evalutate(x_test)
     
--- test.py
+++
@@ -1,8 +0,0 @@
-answer=0
-def test():
-    global answer
-    answer+=1
-    return 0
-test()
-test()
-print(answer)
